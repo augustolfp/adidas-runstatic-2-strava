64bfc331b97489da6aad07208f29b2bb1bd56945
Augusto <augustolfp@gmail.com>
2023-10-23 18:56:54 -0300

    refactor: move batch request function to custom hook

--- src/Components/StatusPanel/index.tsx
+++ src/Components/StatusPanel/index.tsx
@@ -1,66 +1,15 @@
-import { useState } from "react";
-import axios from "axios";
 import SanityCheck from "./SanityCheck";
+import useSendToStrava from "../../hooks/useSendToStrava";
 
 interface Props {
     gpxList: File[];
     accessToken: string;
 }
 
-type POST_File_Request = {
-    status: "success" | "fail";
-    name: string;
-};
-
 export default function StatusPanel({ gpxList, accessToken }: Props) {
-    const [postFileRequests, setPostFileRequests] = useState<
-        POST_File_Request[]
-    >([]);
-
-    const uploadURL = import.meta.env.VITE_UPLOAD_URL;
-
-    const uploadFile = (file: File) => {
-        const formData = new FormData();
-        formData.append("file", file);
-        const randomId = crypto.randomUUID().substring(0, 6);
-
-        const params = {
-            name: `Adidas2Strava import-${randomId}`,
-            description: "No description",
-            trainer: "false",
-            commute: "false",
-            data_type: "gpx",
-            external_id: file.name,
-        };
-
-        const headers = {
-            Authorization: `Bearer ${accessToken}`,
-        };
-
-        const response = axios.post(uploadURL, formData, {
-            headers: { ...headers },
-            params: { ...params },
-        });
-
-        response.then((_res) => {
-            const result: POST_File_Request = {
-                status: "success",
-                name: file.name,
-            };
-            setPostFileRequests((prev) => [...prev, result]);
-        });
-
-        response.catch((_err) => {
-            const result: POST_File_Request = {
-                status: "fail",
-                name: file.name,
-            };
-            setPostFileRequests((prev) => [...prev, result]);
-        });
-    };
-
+    const { batchUpload, results } = useSendToStrava();
     const handleUpload = () => {
-        gpxList.map((file) => uploadFile(file));
+        batchUpload(gpxList, accessToken);
     };
 
     return (
@@ -87,7 +36,7 @@ export default function StatusPanel({ gpxList, accessToken }: Props) {
                         </h3>
                         <div className="bg-base-200 p-2 rounded-lg">
                             <ul className="h-32 overflow-y-scroll p-2">
-                                {postFileRequests.map((result, index) => (
+                                {results.map((result, index) => (
                                     <li
                                         key={index}
                                         className={`${
---
+++ src/hooks/useSendToStrava.ts
@@ -0,0 +1,64 @@
+import { useState } from "react";
+import axios from "axios";
+
+type POST_File_Request = {
+    status: "success" | "fail";
+    name: string;
+};
+
+export default function useSendToStrava() {
+    const [postFileRequests, setPostFileRequests] = useState<
+    POST_File_Request[]
+>([]);
+
+const uploadURL = import.meta.env.VITE_UPLOAD_URL;
+
+const uploadFile = (file: File, accessToken: string) => {
+    const formData = new FormData();
+    formData.append("file", file);
+    const randomId = crypto.randomUUID().substring(0, 6);
+
+    const params = {
+        name: `Adidas2Strava import-${randomId}`,
+        description: "No description",
+        trainer: "false",
+        commute: "false",
+        data_type: "gpx",
+        external_id: file.name,
+    };
+
+    const headers = {
+        Authorization: `Bearer ${accessToken}`,
+    };
+
+    const response = axios.post(uploadURL, formData, {
+        headers: { ...headers },
+        params: { ...params },
+    });
+
+    response.then((_res) => {
+        const result: POST_File_Request = {
+            status: "success",
+            name: file.name,
+        };
+        setPostFileRequests((prev) => [...prev, result]);
+    });
+
+    response.catch((_err) => {
+        const result: POST_File_Request = {
+            status: "fail",
+            name: file.name,
+        };
+        setPostFileRequests((prev) => [...prev, result]);
+    });
+};
+
+const batchUpload = (gpxList: File[], accessToken: string) => {
+    gpxList.map((file) => uploadFile(file, accessToken));
+} 
+
+return {
+    batchUpload,
+    results: postFileRequests
+}
+}
