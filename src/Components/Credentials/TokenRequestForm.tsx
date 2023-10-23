a5c5ee58495382ee13afa29bf0866a7cf3cc780a
Augusto <augustolfp@gmail.com>
2023-10-23 14:34:56 -0300

    feat(credentials): adjust token request form layout

--- src/Components/Credentials/TokenRequestForm.tsx
+++ src/Components/Credentials/TokenRequestForm.tsx
@@ -27,44 +27,68 @@ export default function TokenRequestForm() {
     };
 
     return (
-        <form onSubmit={handleSubmit}>
-            <input
-                name="client_id"
-                placeholder="Client ID"
-                className="input input-bordered"
-                type="number"
-                disabled={isLoading}
-                value={clientId || ""}
-                onChange={(e) => {
-                    if (e.target.value) {
-                        setClientId(parseInt(e.target.value));
-                    }
-                }}
-            />
+        <form
+            onSubmit={handleSubmit}
+            className="flex flex-col gap-2 p-4 bg-base-200 m-4 rounded-xl w-1/2"
+        >
+            <div className="flex flex-col md:flex-row md:justify-between">
+                <label htmlFor="client_id" className="label">
+                    <span className="label-text">Client ID:</span>
+                </label>
+                <input
+                    id="client_id"
+                    name="client_id"
+                    placeholder="Client ID"
+                    className="input input-bordered input-sm md:input-md"
+                    type="number"
+                    disabled={isLoading}
+                    value={clientId || ""}
+                    onChange={(e) => {
+                        if (e.target.value) {
+                            setClientId(parseInt(e.target.value));
+                        }
+                    }}
+                />
+            </div>
 
-            <input
-                name="client_secret"
-                placeholder="Client Secret"
-                className="input input-bordered"
-                type="password"
-                disabled={isLoading}
-                value={clientSecret || ""}
-                onChange={(e) => setClientSecret(e.target.value)}
-            />
+            <div className="flex flex-col md:flex-row md:justify-between">
+                <label htmlFor="client_secret" className="label">
+                    <span className="label-text">Client Secret:</span>
+                </label>
+                <input
+                    id="client_secret"
+                    name="client_secret"
+                    placeholder="Client Secret"
+                    className="input input-bordered input-sm md:input-md"
+                    type="password"
+                    disabled={isLoading}
+                    value={clientSecret || ""}
+                    onChange={(e) => setClientSecret(e.target.value)}
+                />
+            </div>
 
-            <input
-                name="code"
-                placeholder="Code"
-                className="input input-bordered"
-                type="text"
-                disabled={isLoading}
-                value={code || ""}
-                onChange={(e) => setCode(e.target.value)}
-            />
+            <div className="flex flex-col md:flex-row md:justify-between">
+                <label htmlFor="code" className="label">
+                    <span className="label-text">
+                        Code (from previous step):
+                    </span>
+                </label>
+                <input
+                    id="code"
+                    name="code"
+                    placeholder="Code"
+                    className="input input-bordered input-sm md:input-md"
+                    type="text"
+                    disabled={isLoading}
+                    value={code || ""}
+                    onChange={(e) => setCode(e.target.value)}
+                />
+            </div>
+
             <button
                 type="submit"
                 disabled={isLoading}
-                className="btn btn-primary"
+                className="btn btn-primary w-full btn-sm md:btn-md"
             >
                 {isLoading ? "Loading..." : "Send Token request"}
             </button>
