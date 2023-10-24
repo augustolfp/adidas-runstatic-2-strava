fceba9629ef05659ef165b61d483c0e5fc145e3c
Augusto <augustolfp@gmail.com>
2023-10-24 13:05:21 -0300

    refactor: remove GetCredentials wrapper component

--- src/Components/Credentials/index.tsx
+++ src/Components/Credentials/index.tsx
@@ -1,13 +1,14 @@
-import ExplanationText from "./ExplanationText";
-import GetCredentials from "./GetCredentials";
+import GetCode from "./GetCredentials/GetCode";
+import TokenRequestForm from "./GetCredentials/TokenRequestForm";
 
 export default function Credentials() {
     return (
         <div className="card w-full bg-base-100 shadow-xl">
             <div className="card-body">
                 <h2 className="card-title">1. Credentials</h2>
-                <ExplanationText />
-                <GetCredentials />
+
+                <GetCode />
+                <TokenRequestForm />
             </div>
         </div>
     );
