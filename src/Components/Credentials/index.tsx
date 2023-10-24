615d14ad82cd1c4ca568ab1d0d55730dc03b7b1f
Augusto <augustolfp@gmail.com>
2023-10-24 13:33:05 -0300

    refactor: move components common layout into reusable Card component

--- src/Components/Credentials/index.tsx
+++ src/Components/Credentials/index.tsx
@@ -1,19 +1,20 @@
 import useAuth from "../../hooks/useAuth";
+import Card from "../Card";
 import GetCode from "./GetCode";
 import Form from "./Form";
 import FormResult from "./FormResult";
 
 export default function Credentials() {
     const { status, error, result, getAuth } = useAuth();
-    return (
-        <div className="card w-full bg-base-100 shadow-xl">
-            <div className="card-body">
-                <h2 className="card-title">1. Credentials</h2>
 
+    return (
+        <div>
+            <h2 className="card-title">1. Credentials</h2>
+            <Card>
                 <GetCode />
                 <Form onSubmit={getAuth} status={status} />
                 <FormResult status={status} result={result} error={error} />
-            </div>
+            </Card>
         </div>
     );
 }
