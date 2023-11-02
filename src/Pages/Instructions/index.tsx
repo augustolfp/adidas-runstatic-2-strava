9af6e861de305c5bdca3399a23eece67be4e2e0f
Augusto <augustolfp@gmail.com>
2023-11-02 13:49:24 -0300

    refactor: rename instructions md file

--- src/Pages/Instructions/index.tsx
+++ src/Pages/Instructions/index.tsx
@@ -1,13 +1,13 @@
 import ReactMarkdown from "react-markdown";
 import rehypeRaw from "rehype-raw";
-import fileTree from "./fileTree.md";
+import instructions from "./instructions.md";
 
 export default function Instructions() {
     return (
         <div className="container mx-auto max-w-5xl p-8">
-            <div className="prose max-w-none">
+            <div className="prose prose-img:rounded-2xl prose-img:shadow-md max-w-none">
                 <ReactMarkdown
-                    children={fileTree}
+                    children={instructions}
                     rehypePlugins={[rehypeRaw]}
                 />
             </div>
